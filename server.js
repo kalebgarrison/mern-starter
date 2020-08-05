4208d7079997cb355c400c591d07af1a63ae1a10
kalebgarrison <kalebgarrison@gmail.com>
2020-08-04 20:16:45 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -8,24 +9,30 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
 });
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/mern-starter",
-  {
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/mern-starter", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-  }
-).then(() => {
-  console.log("Successfully connected to database.");
-}).catch((err) => {
-  console.log("Unable to connect to database.");
-  console.log(err);
-});
+  })
+  .then(() => {
+    console.log("Successfully connected to database.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to database.");
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Express server running on http://localhost:${PORT}`);
